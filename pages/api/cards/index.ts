13c709ae675c563f008e225eebc74d23650f2094
Jason Xu <Xinwen31415@gmail.com>
2020-05-11 13:33:09 -0500

    Redeploy

--- pages/api/cards/index.ts
+++ pages/api/cards/index.ts
@@ -3,35 +3,38 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	console.log(process.env);
-	const dbURL = process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017' : `mongodb+srv://Me:${process.env.MONGODB_PASSWORD}@quiz-cards-cluster-hwc6f.mongodb.net/test`;
-	try {
-		const client = await MongoClient.connect(dbURL, { useUnifiedTopology: true });
-		const db = client.db('cards');
-		const cards = await db.collection('cards').find({}).toArray();
-		switch (req.method) {
-			case ('POST'):
-				try {
-					const { category, subcategory, hint, answer } = req.body;
-					
-					await db.collection('cards').insertOne({
-						category,
-						hint,
-						answer,
-						subcategory: subcategory === '' ? null : subcategory
-					});
-					res.status(200).send('Card Created!');
-				}
-				catch (err) {
-					console.log(err);
-					res.status(500).send('Server Error: DB Failed to push');
-				}
+	const dbURL = process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017' : `mongodb+srv://Me:${process.env.MONGODB_PASSWORD}@quiz-cards-cluster-hwc6f.mongodb.net/test?retryWrites=true&w=majority`;
+	switch (req.method) {
+		case ('POST'):
+			try {
+				const { category, subcategory, hint, answer } = req.body;
+				const client = await MongoClient.connect(dbURL, { useUnifiedTopology: true });
+				const db = client.db('cards');
 				
-				return;
-			case ('GET'):
+				await db.collection('cards').insertOne({
+					category,
+					hint,
+					answer,
+					subcategory: subcategory === '' ? null : subcategory
+				});
+				res.status(200).send('Card Created!');
+			}
+			catch (err) {
+				console.log(err);
+				res.status(500).send('Server Error: DB Failed to push');
+			}
+			
+			return;
+		case ('GET'):
+			try {
 				if (!req.query.categories) {
 					res.status(400).send('Must have category');
 					return;
 				}
+				const client = await MongoClient.connect(dbURL, { useUnifiedTopology: true });
+				const db = client.db('cards');
+				const cards = await db.collection('cards').find({}).toArray();
+				
 				const categories = [];
 	
 				if (typeof req.query.categories === 'string') {
@@ -45,14 +48,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 				const response: Card[] = cards.filter((card) => categories.includes(card.category));
 	
 				res.status(200).json(response);
-				return;
-			default:
-				res.status(405).end();
-				return;
-		}
-	}
-	catch (err) {
-		console.log(err);
-		res.status(500).send('DB Failed to Connect');
+			}
+			catch (err) {
+				console.log(err);
+				res.status(500).send('DB Failed to Connect');
+			}
+			return;
+		default:
+			res.status(405).end();
+			return;
 	}
 }
