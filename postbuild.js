0d6206198ff8d7e525331b7b4805b6d66f623f62
Jason Xu <Xinwen31415@gmail.com>
2020-05-11 02:44:11 -0500

    Redeploy

--- postbuild.js
+++ postbuild.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
-console.log(process.cwd());
-console.log(fs.readdirSync(`${process.cwd()}/.next/serverless/pages/api`));
+// console.log(process.cwd());
+// console.log(fs.readdirSync(`${process.cwd()}/.next/serverless/pages/api`));
 
-fs.copyFileSync(`${process.cwd()}/cards.json`, `${process.cwd()}/.next/serverless/pages/api/cards.json`);
+// fs.copyFileSync(`${process.cwd()}/cards.json`, `${process.cwd()}/.next/serverless/pages/api/cards.json`);
+
+fs.copyFileSync(`${process.cwd()}/cards.json`, `${process.cwd()}/.next/cards.json`);
