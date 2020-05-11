e1f4930eee8a65a5759d6f5325e5e56dab4649b5
Jason Xu <Xinwen31415@gmail.com>
2020-05-11 02:19:38 -0500

    Redeploy

--- postbuild.js
+++ postbuild.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-console.log(fs.readdirSync(`${process.cwd()}/.next`));
+console.log(fs.readdirSync(`${process.cwd()}/.next/serverless`));
 
-const folders = fs.readdirSync(`${process.cwd()}/.next/server/static`);
+const folders = fs.readdirSync(`${process.cwd()}/.next/serverless/static`);
 
-const dest =  folders.filter((file) => fs.statSync(`${process.cwd()}/.next/server/static/${file}`).isDirectory() && file !== 'development')[0];
+const dest =  folders.filter((file) => fs.statSync(`${process.cwd()}/.next/serverless/static/${file}`).isDirectory() && file !== 'development')[0];
 
-fs.copyFileSync(`${process.cwd()}/cards.json`, `${process.cwd()}/.next/server/static/${dest}/cards.json`);
+fs.copyFileSync(`${process.cwd()}/cards.json`, `${process.cwd()}/.next/serverless/static/${dest}/cards.json`);
