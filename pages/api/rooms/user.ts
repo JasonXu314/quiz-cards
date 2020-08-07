356ec99fda8819fac11ddce3d0d4d206caa276f0
Jason Xu <Xinwen31415@gmail.com>
2020-08-07 03:51:14 -0500

    Fixed pusher

--- pages/api/rooms/user.ts
+++ pages/api/rooms/user.ts
@@ -1,18 +1,17 @@
 import { MongoClient } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
-import Pusher from 'pusher';
 import { v4 as uuid } from 'uuid';
 
 export default async (req: NextApiRequest, res: NextApiResponse<CreateUserResponse | string>): Promise<void> => {
 	try {
 		const client = await MongoClient.connect(process.env.MONGODB_URL!, { useUnifiedTopology: true });
 		const db = client.db('rooms');
-		const pusher = new Pusher({
-			key: 'fe3b8164d0dd3aff71b6',
-			appId: '1026188',
-			cluster: 'us2',
-			secret: '5fa75bf9b0d4fcb17a99'
-		});
+		// const pusher = new Pusher({
+		// 	key: 'fe3b8164d0dd3aff71b6',
+		// 	appId: '1026188',
+		// 	cluster: 'us2',
+		// 	secret: '5fa75bf9b0d4fcb17a99'
+		// });
 
 		switch (req.method) {
 			case 'POST':
@@ -20,23 +19,23 @@ export default async (req: NextApiRequest, res: NextApiResponse<CreateUserRespon
 
 				res.status(200).json({ user: { ...createdUser, id: createdUser.id } });
 				break;
-			case 'PATCH':
-				const { id, name }: { id: string; name: string } = req.body;
+			// case 'PATCH':
+			// 	const { id, name }: { id: string; name: string } = req.body;
 
-				await db.collection<IUser>('roomUsers').findOneAndUpdate({ id: id }, { $set: { name } });
-				const leaderboards = (await db.collection<ILeaderboard>('leaderboards').find().toArray()).filter((lbd: ILeaderboard) =>
-					lbd.leaderboard.some((user) => user.id === id)
-				);
-				leaderboards.forEach((lbd) => {
-					db.collection<ILeaderboard>('leaderboards').findOneAndUpdate(
-						{ roomId: lbd.roomId },
-						{ $set: { leaderboard: lbd.leaderboard.map((user) => (user.id === id ? { ...user, name } : user)) } }
-					);
-				});
+			// 	await db.collection<IUser>('roomUsers').findOneAndUpdate({ id: id }, { $set: { name } });
+			// 	const leaderboards = (await db.collection<ILeaderboard>('leaderboards').find().toArray()).filter((lbd: ILeaderboard) =>
+			// 		lbd.leaderboard.some((user) => user.id === id)
+			// 	);
+			// 	leaderboards.forEach((lbd) => {
+			// 		db.collection<ILeaderboard>('leaderboards').findOneAndUpdate(
+			// 			{ roomId: lbd.roomId },
+			// 			{ $set: { leaderboard: lbd.leaderboard.map((user) => (user.id === id ? { ...user, name } : user)) } }
+			// 		);
+			// 	});
 
-				pusher.trigger('quiz-cards-rooms', 'NAME_CHANGE', { userId: id, name });
-				res.status(200).send('Name Updated');
-				break;
+			// 	pusher.trigger('quiz-cards-rooms', 'NAME_CHANGE', { userId: id, name });
+			// 	res.status(200).send('Name Updated');
+			// 	break;
 			default:
 				res.status(405).send('Method not Allowed');
 				break;
